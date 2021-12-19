ace3762ac9384f92d1cd179fc3db02044e4c4fc3
Milosz Piglas <milosz@archeocs.com>
2021-12-19 11:41:22 +0100

    fix: format class

--- procj-common/src/main/java/org/procj/coerce/FromInstant.java
+++ procj-common/src/main/java/org/procj/coerce/FromInstant.java
@@ -8,28 +8,28 @@ import java.time.ZoneId;
 
 public class FromInstant {
 
-	public static LocalDateTime toLocalDateTime(Instant i) {
-		return LocalDateTime.ofInstant(i, ZoneId.of("UTC"));
-	}
-	
-	public static LocalDate toLocalDate(Instant i) {
-		return toLocalDateTime(i).toLocalDate();
-	}
-	
-	public static OffsetDateTime toOffsetDateTime(Instant i) {
-		return OffsetDateTime.ofInstant(i, ZoneId.of("UTC"));
-	}
-	
-	// toUtilDate
-	
-	// toSqsDate
-	
-	// toTimestamp
-	
-	// toLocalDate
-	
-	// toOffsetDateTime
-	
-	// toInstant
-	
+  public static LocalDateTime toLocalDateTime(Instant i) {
+    return LocalDateTime.ofInstant(i, ZoneId.of("UTC"));
+  }
+
+  public static LocalDate toLocalDate(Instant i) {
+    return toLocalDateTime(i).toLocalDate();
+  }
+
+  public static OffsetDateTime toOffsetDateTime(Instant i) {
+    return OffsetDateTime.ofInstant(i, ZoneId.of("UTC"));
+  }
+
+  // toUtilDate
+
+  // toSqsDate
+
+  // toTimestamp
+
+  // toLocalDate
+
+  // toOffsetDateTime
+
+  // toInstant
+
 }
